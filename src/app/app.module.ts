156f8d67efa675c08d0617693505be8065c50cd0
Glennkh <hglen93@gmail.com>
2017-07-03 13:26:13 -0700

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HttpModule } from '@angular/http';
+
+import { ProfilesPage } from '../pages/profiles/profiles';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+import { RepoDetailsPage } from '../pages/repo-details/repo-details';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { GithubServiceProvider } from '../providers/github-service/github-service';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    ProfilesPage,
+    ContactPage,
+    HomePage,
+    TabsPage,
+    RepoDetailsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    HttpModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    ProfilesPage,
+    ContactPage,
+    HomePage,
+    TabsPage,
+    RepoDetailsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    GithubServiceProvider
+  ]
+})
+export class AppModule {}
---
+++ src/pages/profiles/profiles.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { GithubServiceProvider} from '../../providers/github-service/github-service';
+import { RepoDetailsPage } from '../repo-details/repo-details';
+/**
+ * Generated class for the ProfilesPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
+@Component({
+  selector: 'page-profiles',
+  templateUrl: 'profiles.html',
+})
+export class ProfilesPage {
+
+  profiles: any;
+  repos: any;
+  github_user = "";
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private githubServiceProvider: GithubServiceProvider) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ProfilesPage');
+  }
+
+  onSubmit(){
+    this.getProfile(this.github_user);
+    this.github_user = '';
+  }
+
+  reset(){
+      this.profiles = '';
+      this.github_user = '';
+      this.repos = '';
+  }
+
+  showRepos(github_user){
+    this.getRepos(github_user);
+  }
+
+  repoTapped(event, repo){
+    this.navCtrl.push(RepoDetailsPage,{
+      repo: repo
+    });
+  }
+
+  getProfile(username){
+    this.githubServiceProvider.getProfile(username).subscribe(response => {
+      this.profiles = response;
+      console.log(this.profiles);
+    })
+  }
+
+  getRepos(username){
+    this.githubServiceProvider.getRepos(username).subscribe(response =>{
+      this.repos = response;
+      console.log(this.repos);
+    })
+  }
+
+}
